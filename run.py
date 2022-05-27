6b213baa0457257d6a5b35ea157352d7d8c143db
RyanMAWalker <rmawalk@hotmail.com>
2022-05-27 17:32:16 +0000

    Add user option statement

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import os
+clear = lambda: os.system('cls') # taken from https://www.codegrepper.com/code-examples/python/how+to+make+a+clear+function+python
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,7 +17,8 @@ SHEET = GSPREAD_CLIENT.open('inventory_management_RW')
 
 def get_user_info():
     """
-    Get user name and role from user
+    Get user name and role from user.
+    Runs while loop to collect a valid string from the user.
     """
     while True: 
         first_name_str = input("Enter your first name: \n")
@@ -25,7 +29,7 @@ def get_user_info():
             print(f"Hello, {first_name_str.capitalize()} {last_name_str.capitalize()}")
             print(f"Your job role is {job_role_str.capitalize()} and can now access the inventory")
             break
-
+    return validate_role(job_role_str)
         
 
 def validate_role(role):
@@ -36,7 +40,7 @@ def validate_role(role):
     try:
         if role.lower() != "manager":
             raise TypeError(
-                f"Only a Manager may access this doccument.\n"
+                "Only a Manager may access this doccument.\n"
             )
     except TypeError as e:
         print(f"Invalid job role: {e} please try again.\n")
@@ -44,4 +48,23 @@ def validate_role(role):
 
     return True
 
-get_user_info()
+def user_options():
+    """
+    User options for adding items or viewing current inventory
+    """
+    clear()
+    print("Welcome to Inventory Management.")
+    print("Please select an option below:\n")
+    print("1 - Add Stock")
+    print("2 - View Stock")
+
+#def add_stock_to_inventory():
+
+def main(): 
+    """
+    Run all program functions
+    """
+    user_info = get_user_info()
+    options = user_options()
+
+main()
