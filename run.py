2028d3f7ffc0e62ab998d89af278b447845ccbbe
RyanMAWalker <rmawalk@hotmail.com>
2022-05-27 18:44:34 +0000

    Add functionality to stock add option

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-import os
-clear = lambda: os.system('cls') # taken from https://www.codegrepper.com/code-examples/python/how+to+make+a+clear+function+python
+import subprocess
 
 
 SCOPE = [
@@ -21,12 +20,11 @@ def get_user_info():
     Runs while loop to collect a valid string from the user.
     """
     while True: 
-        first_name_str = input("Enter your first name: \n")
-        last_name_str = input("Enter your last name: \n")
+        name_str = input("Enter your full name: \n")
         job_role_str = input("Enter your job role:")
 
         if validate_role(job_role_str):
-            print(f"Hello, {first_name_str.capitalize()} {last_name_str.capitalize()}")
+            print(f"Hello, {name_str.capitalize()}")
             print(f"Your job role is {job_role_str.capitalize()} and can now access the inventory")
             break
     return validate_role(job_role_str)
@@ -37,13 +35,17 @@ def validate_role(role):
     Inside this try, it checks that the role must be a Manager.
     Raises a TypeError if the string is incorrect.
     """
+    print("Validate roll being called")
     try:
-        if role.lower() != "manager":
+        role = role.lower()
+        print(role)
+        if role not in ["manager", "supervisor"]:
+            print("here")
             raise TypeError(
-                "Only a Manager may access this doccument.\n"
+                "Only a Manager & Supervisor may access this doccument.\n"
             )
     except TypeError as e:
-        print(f"Invalid job role: {e} please try again.\n")
+        print(e)
         return False
 
     return True
@@ -52,21 +54,57 @@ def user_options():
     """
     User options for adding items or viewing current inventory
     """
-    clear()
+    subprocess.run("clear")
     print("Welcome to Inventory Management.")
     print("Please select an option below:\n")
     print("1 - Add Stock")
     print("2 - View Stock")
     
-    choice = input("Choose option '1' or '2': ")    
-    if choice == "1":
-        add_stock()
-    elif choice == "2":
-        view_stock()
+    while True:
+        choice = input("Choose option '1' or '2': ")    
+        if choice == "1":
+            add_stock()
+            break
+        elif choice == "2":
+            view_stock()
+            break
 
 def add_stock():
-    print("add Stock")
+    subprocess.run("clear")
+    print("Add Stock".upper())
+    print("Please select an option below:\n")
+    print("1 - Add One Item")
+    print("2 - Add Multiple Items\n")
 
+    while True:
+        choice = input("Choose option '1' or '2': ")    
+        if choice in ["1" , "2"]:
+            break
+    if choice == "1":
+        print()
+        while True:
+            add_num_items = input("Please enter the number of items to be added: ")
+            if add_num_items.isdigit():
+                break
+        add_num_items = int(add_num_items)
+        num_items = {}
+        for i in range(1, add_num_items+1):
+            while True:
+                print()
+                name_item = input("Item Name: ")
+                if name_item != "":
+                    break
+            while True:
+                item_quantity = input("Quanitity: ")
+                if item_quantity.isdigit():
+                    break
+            while True:
+                item_cost = input("Enter Value (GBP): ")
+                if item_cost.isdigit():
+                    break
+            num_items.update({name_item, int(item_quantity), float(item_cost)})
+
+    
 def view_stock():
     print("view Stock")
 
