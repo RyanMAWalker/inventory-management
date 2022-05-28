8cfc8ff54adb58c550fd6a334d3630a414c5d1e9
RyanMAWalker <rmawalk@hotmail.com>
2022-05-28 02:13:23 +0000

    Change add stock funtion for be more readable, remove view stock function, remove add items function

--- run.py
+++ run.py
@@ -14,14 +14,8 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('inventory_management_RW')
 
-def isfloat(num):
-    try:
-        float(num)
-        return True
-    except ValueError:
-        return False
-
-
+stock = SHEET.worksheet('stock')
+data = stock.get_all_values()
 
 def get_user_info():
     """
@@ -30,11 +24,13 @@ def get_user_info():
     """
     while True: 
         name_str = input("Enter your full name: \n")
-        job_role_str = input("Enter your job role:")
+        print("Only a Manager or Supervisor may access this document.")
+        print("Please enter your position below")
+        job_role_str = input("Enter your position here:")
 
         if validate_role(job_role_str):
             print(f"Hello, {name_str.capitalize()}")
-            print(f"Your job role is {job_role_str.capitalize()} and can now access the inventory")
+            print(f"Your job role is {job_role_str.capitalize()} and can now access the inventory.")
             break
     return validate_role(job_role_str)
         
@@ -75,108 +71,28 @@ def user_options():
             view_stock()
             break
 
-def add_single_item():
-    while True:
-        item_name = input("Item Name: ")
-        if item_name != "":
-            break
-    while True:
-        item_quantity = input("Quantity: ")
-        if item_quantity.isdigit():
-            break
-    while True:
-        item_value = input("Value: ")
-        if isfloat(item_value):
-            break
-
-def add_multiple_items():
-    while True:
-        number_of_items = input("Enter the number of items to be added: ")
-        if number_of_items.isdigit():
-            break
-        number_of_items = int(number_of_items)
-        user_items = {}
-        for i in range(1, number_of_items+1):
-            while True:
-                print()
-                item_name = input("Item Name: ")
-                if item_name != "":
-                    break
-            while True:
-                item_quantity = input("Item Quantity: ")
-                if item_quantity.isdigit():
-                    break
-            while True:
-                item_value = input("Value: ")
-                if isfloat(item_value):
-                    break
-
 def add_stock():
-    """
-    User options for adding one item, or adding multiple.
-    """
-    subprocess.run("clear")
-    print("Add Stock".upper())
-    print("Please select an option below:\n")
-    print("1 - Add One Item")
-    print("2 - Add Multiple Items\n")
+    print("Please enter the data as the following:")
+    print("Item, Quantity, Price\n")
+    print("For Example:")
+    print("Shoes, 2, 26.50")
 
-    while True:
-        choice = input("Choose an option: ")
-        if choice in ['1', '2']:
-            break
-    if choice == '1':
-        print()
-        add_single_item()
+    stock_str = input("Enter the data here: ")
+    print(f"You have provided: {stock_str}")
+
+    stock_data = stock_str.split(",")
+    validate_stock(stock_data)
+
+def validate_stock(values):
+    print(values)
 
 
-    elif choice == '2':
-        print()
-        add_multiple_items()
-        user_items.update({item_name: int(item_quantity)})
-
-        addItemsToFile(user_items, clear=False)
-
-    
-def view_stock():
-    """
-    User option for viewing current stock list.
-    Edit and delete functionaility.
-    """
-    subprocess.run("clear")
-    print("View Stock")   
-    print("Choose and Option:")
-    print("1 - Edit Item")
-    print("2 - Delete Item")
-    print()
-    while True:
-        choice = input("Choose option '1' or '2': ")    
-        if choice == "1":
-            print("This will edit")
-            edit_item()
-            break
-        elif choice == "2":
-            print("this will delete")
-            delete_item()
-            break
-
- 
-def add_stock_to_sheet(add_stock):
-    """
-    Update worksheet with the input stock data to add a new row
-    """
-    print("Updating worksheet")
-    stock_worksheet = SHEET.worksheet("stock")
-    stock_worksheet.append_row(add_stock)
-    print("Stock worksheet updated")
 
 
-def main(): 
-    """
-    Run all program functions
-    """
-    user_info = get_user_info()
-    options = user_options()
-    add_stock_to_sheet(add_stock())
+def main():
+
+    get_user_info()
+    user_options()
+    add_stock()
 
 main()
